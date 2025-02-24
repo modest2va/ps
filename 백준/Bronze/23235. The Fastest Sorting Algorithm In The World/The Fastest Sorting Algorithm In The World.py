43d029571bee4bb469e26c489f0ea5e442cee762
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 23:00:23 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    cnt = 1
+    while True:
+        s =input()
+        if s =='0':
+            break
+        else:
+            print("Case %d: Sorting... done!"%(cnt))
+        cnt+=1
