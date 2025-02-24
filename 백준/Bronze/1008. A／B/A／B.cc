de3aac31319d0c9a965c65e9143c14812420fa57
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 21:28:35 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+double a,b;
+string tmp;
+cin>>a;
+cin>>b;
+
+printf("%.32f",a/b);
+return 0;
+}
