812f63e46c34a7c800888efd47faf0ae58463971
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:52:10 +0900

    [Bronze II] Title: 나의 학점은?, Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/17826. 나의 학점은？/나의 학점은？.py
@@ -0,0 +1,9 @@
+tmp= (list(map(int, input().split())))
+ans=tmp.index(int(input()))+1
+if ans<6: print("A+")
+elif ans<16 : print("A0")
+elif ans<31 : print("B+")
+elif ans<36 : print("B0")
+elif ans<46 : print("C+")
+elif ans<49 : print("C0")
+else: print("F")
