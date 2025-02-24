34ab3bcade0d69f44648d9dfe6331f5033daab20
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 23:10:43 +0900

    [Bronze V] Title: Serca, Time: 400 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,15 @@
+def sol(n):
+    for _ in range(n):
+        print(' @@@   @@@ ')
+        print('@   @ @   @')
+        print('@    @    @')
+        print('@         @')
+        print(' @       @ ')
+        print('  @     @  ')
+        print('   @   @   ')
+        print('    @ @    ')
+        print('     @     ')
+
+if __name__ == '__main__':
+    n = int(input())
+    sol(n)
