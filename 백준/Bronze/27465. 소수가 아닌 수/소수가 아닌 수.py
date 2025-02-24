af5eca12d960cc9d6519437b7ace28cb7e917554
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 23:12:07 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
@@ -0,0 +1,11 @@
+def sol(n):
+    return 10**9
+    # if n % 2 == 0:
+    #     return n + 2
+    # else:
+    #     return n + 3
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(sol(n))
