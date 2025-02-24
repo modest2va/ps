63cabc6a660afb5a86eb12aeb53c288ae3acdf78
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 21:29:22 +0900

    [Bronze II] Title: 저항, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,5 @@
+colors=['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+a=colors.index(input())
+b=colors.index(input())
+c=colors.index(input())
+print( ( a*10+b)*(10**c) )
