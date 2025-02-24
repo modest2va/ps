0d2db7900c4a0709ace821dc7b702ef349b43a34
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:26:55 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1992 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+int a,b;
+string tmp;
+cin>>a;
+cin>>b;
+
+cout<<a*b;
+
+return 0;
+}
