f9389cfd7d86f8360e84a1c1ea209125ee139f49
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 23:15:42 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,17 @@
+# def sol(s):
+#     return x + y + 3
+
+
+if __name__ == '__main__':
+    for i in range(15):
+        s = input()
+        if 'w' in s:
+            print('chunbae')
+            break
+        elif 'b' in s:
+            print('nabi')
+            break
+        elif 'g' in s:
+            print('yeongcheol')
+            break
+            
