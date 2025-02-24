448547ac8f34df4dbdcf45d525f05d9073e16e1e
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:56:36 +0900

    [Bronze IV] Title: Archivist, Time: 64 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,12 @@
+winners=[]
+for i in range(25): winners.append('ITMO')
+winners[1]='SPbSU'
+winners[2]='SPbSU'
+winners[5]='SPbSU'
+winners[11]='PetrSU, ITMO'
+winners[12]='SPbSU'
+winners[13]='SPbSU'
+winners[23]='SPbSU'
+winners[18]='SPbSU'
+n=int(input())-1995
+print(winners[n])
