29654b15e91d115e35ab0d600e3d24f64e2a09dc
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 23:01:58 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,12 @@
+def print1(n):
+    for i in range(n):
+        print('@'*n*5)
+def print2(n):
+    for i in range(n):
+        print('@'*n)
+n=int(input())
+print1(n)
+print2(n)
+print1(n)
+print2(n)
+print2(n)
