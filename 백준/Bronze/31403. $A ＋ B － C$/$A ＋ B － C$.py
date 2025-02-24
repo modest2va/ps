2139ebaec23b8e2d909e236528536281beddd18b
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 23:16:32 +0900

    [Bronze IV] Title: $A + B - C$, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,10 @@
+def sol(a, b, c):
+    print(int(a) + int(b) - int(c))
+    print(int(a+b) - int(c))
+
+
+if __name__ == '__main__':
+    a = input()
+    b = input()
+    c = input()
+    sol(a, b, c)
