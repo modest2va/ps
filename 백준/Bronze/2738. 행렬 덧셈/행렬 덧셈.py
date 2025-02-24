f4eecba59efab7bbdb928ddbe31191fe2c72bc6a
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 21:53:06 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+a = []
+b = []
+for k in range(n):
+    a_list = list(map(int, input().split()))
+    a.append(a_list)
+for k in range(n):
+    b_list = list(map(int, input().split()))
+    b.append(b_list)
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j] + b[i][j], end = ' ')
+    print()
