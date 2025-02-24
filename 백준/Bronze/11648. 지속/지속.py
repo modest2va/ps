4b2e8a1ecaca93c9192782f1103c1358306eeb6f
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:31:39 +0900

    [Bronze III] Title: 지속, Time: 72 ms, Memory: 29076 KB -BaekjoonHub

---
+++ 백준/Bronze/11648. 지속/지속.py
@@ -0,0 +1,13 @@
+def multi_digit(n):
+    ans=1
+    for i in str (n):
+        ans*=int(i)
+    return ans
+
+n=input()
+cnt=0
+while int (n)>=10:
+    if int(n)<10: break
+    n= multi_digit(n)
+    cnt+=1
+print(cnt)
