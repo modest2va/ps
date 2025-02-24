8ba46b3d4b637a058f60ae313a51f1a42e58c06a
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 21:53:26 +0900

    [Bronze IV] Title: 기찍 N, Time: 16 ms, Memory: 1992 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+int N;
+
+scanf("%d",&N);
+for(int i=N;i>0;i--){
+    printf("%d\n",i);
+}
+return 0;
+}
