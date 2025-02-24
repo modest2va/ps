1ffec8037d3b316ef076566129e9a3bbb99df0fb
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:44:07 +0900

    [Bronze III] Title: 헬멧과 조끼, Time: 76 ms, Memory: 29088 KB -BaekjoonHub

---
+++ 백준/Bronze/15781. 헬멧과 조끼/헬멧과 조끼.py
@@ -0,0 +1,5 @@
+import sys
+dumm , dummy=map(int, sys.stdin.readline().split())
+tmp1=list(map(int, sys.stdin.readline().split()))
+tmp2=list(map(int, sys.stdin.readline().split()))
+print(max(tmp1)+max(tmp2))
