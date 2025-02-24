a3915db8fa34ac72ba1d347b90f98cabbbfc8c26
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:27:20 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int  N;
+cin>>N;
+for(int i=1;i<N+1;i++){
+    int tmp1,tmp2;
+    cin>>tmp1>>tmp2;
+    cout<<"Case #"<<i<<": "<<tmp1+tmp2<<endl;
+    }
+return 0;
+}
+
+
+
+
