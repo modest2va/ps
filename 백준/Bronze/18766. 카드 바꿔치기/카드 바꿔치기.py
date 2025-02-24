5e4db3327b3eaaa42aa78b1a28679db7a513a168
modest2va <83801385+modest2va@users.noreply.github.com>
2025-02-24 22:54:30 +0900

    [Bronze I] Title: 카드 바꿔치기, Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/18766. 카드 바꿔치기/카드 바꿔치기.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    n=int(input())
+    tmp1=list(map(str, input().split()))
+    tmp2=list(map(str, input().split()))
+    if sorted(tmp1)==sorted(tmp2) : print("NOT CHEATER")
+    else: print("CHEATER")
